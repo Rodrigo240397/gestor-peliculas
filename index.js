481e5d801ca5a016634231922fae4dd937004a08
Rodrigo <rodrigo.figueroa.pacheco@gmail.com>
2026-05-25 10:52:46 +0200

    configurar ejs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import expressLayouts from 'express-ejs-layouts';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -7,6 +8,16 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Establecer el motor de vistas a EJS
+app.set('view engine', 'ejs');
+app.set("view options", { root: "views" })
+// Configurar express-ejs-layouts
+app.use(expressLayouts);
+app.set('layout', 'layout/default-layout'); // Ruta al layout principal
+app.set("layout extractScripts", true)
+app.set("layout extractStyles", true)
+app.set("layout extractMetas", true)
+
 // Servir archivos estáticos desde la carpeta "public"
 app.use(express.static('public'));
 // Servir los archivos de Bootstrap como estáticos
@@ -14,7 +25,7 @@ app.use('/css/bootstrap', express.static(path.join(__dirname, 'node_modules/boot
 app.use('/js/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.render('index', { title: 'Inicio' });
 })
 
 app.listen(3000, (error) => {

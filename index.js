7aedf29c1c064fe69ec15c63739763bdffab6e2a
Rodrigo <rodrigo.figueroa.pacheco@gmail.com>
2026-05-25 10:36:26 +0200

    instalar bootstrap y servir los archivo de bootstrap desde la carpeta public

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-import express from 'express'
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const app = express()
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Servir archivos estáticos desde la carpeta "public"
+app.use(express.static('public'));
+// Servir los archivos de Bootstrap como estáticos
+app.use('/css/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 
 app.get('/', (req, res) => {
   res.send('Hello World')

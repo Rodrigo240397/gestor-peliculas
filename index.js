8029ecd4b35ff12324c56410af07517252f9a7f3
Rodrigo <rodrigo.figueroa.pacheco@gmail.com>
2026-05-25 10:30:08 +0200

    inicializar servidor express

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(3000, (error) => {
+  if (error) {
+    console.error('Error starting server:', error.message)
+    return
+  }
+  console.log('Server is running on http://localhost:3000')
+})
